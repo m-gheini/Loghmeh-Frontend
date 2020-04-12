82461ae247c90834e1e1e44b11dbc82c000a6ca4
leilafakheri <fakheri90@gmail.com>
2020-04-12 13:01:27 +0430

    Choice tag added

--- src/js/credit.js
+++ src/js/credit.js
@@ -32,43 +32,18 @@ export class Credit extends React.Component{
         return (
             <div className="whole">
                 <Menu location="userInfo"/>
-                <div className="container-fluid user-info pink-back" dir="rtl" lang="fa">
-                    <div className="col-sm-6 important" dir="rtl" lang="fa"><span className="flaticon-account " lang="fa"/>احسان
-                        خامس پناه
-                    </div>
-                    <div className="col-sm-6 info" dir="rtl" lang="fa">
-                        <ul lang="fa">
-                            <li lang="fa"><i className="flaticon-phone" dir="rtl"/>
-                                ۰۹۱۲۳۴۵۶۷۸۹
-                            </li>
-                            <li lang="fa"><i className="flaticon-mail" dir="rtl"/>
-                                ekhamespanah@yahoo.com
-                            </li>
-                            <li lang="fa"><i className="flaticon-card" dir="rtl"/>
-                                ۱۰۰۰۰۰ تومان
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <UserInfo/>
                 <div className="container-fluid credit-content" lang="fa">
-
                     <div className="container-fluid col-sm-10 box white-back" lang="fa">
-    <span className="container btn-group col-sm-8 choices white-back" dir="rtl" lang="fa">
-        <button type="button" className="btn btn-primary orders pink-back white-font" dir="rtl"
-                onClick={this.goToCredit} lang="fa">افزایش اعتبار</button>
-        <button type="button" className="btn btn-primary credit white-back black-font left-border" dir="rtl"
-                onClick={this.goToOrders} lang="fa">سفارش ها</button>
-    </span>
-
+                        <Choices location="credit"/>
                         <div className="inner" lang="fa">
                             <form className="form-inline" dir="rtl" lang="fa">
-            <span className="form-input" dir="rtl" lang="fa">
-                    <input type="text" className="form-control gray-back down" placeholder="میزان افزایش اعتبار"
-                           size="30" dir="rtl" lang="fa"/>
-            </span>
+                                <span className="form-input" dir="rtl" lang="fa">
+                                    <input type="text" className="form-control gray-back down" placeholder="میزان افزایش اعتبار" size="30" dir="rtl" lang="fa"/>
+                                </span>
                                 <span className="form-btn" dir="rtl" lang="fa">
-                <button type="submit" className="btn submit-btn down" dir="rtl" lang="fa">افزایش</button>
-            </span>
+                                    <button type="submit" className="btn submit-btn down" dir="rtl" lang="fa">افزایش</button>
+                                </span>
                             </form>
                         </div>
                     </div>
@@ -77,5 +52,67 @@ export class Credit extends React.Component{
             </div>
         );
     }
+
+}
+export class UserInfo extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container-fluid user-info pink-back" dir="rtl" lang="fa">
+                <div className="col-sm-6 important" dir="rtl" lang="fa"><span className="flaticon-account " lang="fa"/>احسان
+                    خامس پناه
+                </div>
+                <div className="col-sm-6 info" dir="rtl" lang="fa">
+                    <ul lang="fa">
+                        <li lang="fa"><i className="flaticon-phone" dir="rtl"/>
+                            ۰۹۱۲۳۴۵۶۷۸۹
+                        </li>
+                        <li lang="fa"><i className="flaticon-mail" dir="rtl"/>
+                            ekhamespanah@yahoo.com
+                        </li>
+                        <li lang="fa"><i className="flaticon-card" dir="rtl"/>
+                            ۱۰۰۰۰۰ تومان
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+export class Choices extends React.Component{
+    constructor(props) {
+        super(props);
+        this.goToCredit = this.goToCredit.bind(this);
+        this.goToOrders = this.goToOrders.bind(this);
+    }
+    goToOrders(){
+        ReactDOM.render(<Orders />,document.getElementById("root"));
+    }
+    goToCredit(){
+        ReactDOM.render(<Credit />,document.getElementById("root"));
+    }
+
+    render() {
+        return (
+            <div>
+            {this.props.location==="credit" &&
+            <span className="container btn-group col-sm-8 choices white-back" dir="rtl" lang="fa">
+                    <button type="button" className="btn btn-primary orders pink-back white-font" dir="rtl" onClick={this.goToCredit} lang="fa">افزایش اعتبار</button>
+                    <button type="button" className="btn btn-primary credit white-back black-font left-border" dir="rtl" onClick={this.goToOrders} lang="fa">سفارش ها</button>
+            </span>
+            }
+            {this.props.location === "orders" &&
+            <span className="container btn-group col-sm-8 choices pink-back" dir="rtl" lang="fa">
+                <button type="button" className="btn btn-primary orders black-font white-back" dir="rtl" onClick={this.goToCredit} lang="fa">افزایش اعتبار</button>
+                <button type="button" className="btn btn-primary credit white-font pink-back left-border" dir="rtl" onClick={this.goToOrders} lang="fa">سفارش ها</button>
+            </span>
+            }
+            </div>
+        );
+    }
+
 
 }
--- src/js/orders.js
+++ src/js/orders.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import logo from '../Assets/LOGO.png';
-import {Credit} from "./credit";
+import {Choices, Credit, UserInfo} from "./credit";
 import {Home} from "./home";
 import {SignIn} from "./signIn";
 import {Footer} from "./footer";
@@ -31,53 +31,28 @@ export class Orders extends React.Component{
         return (
             <div className="whole">
                 <Menu location="userInfo"/>
-                <div className="container-fluid user-info pink-back" dir="rtl" lang="fa">
-                    <div className="col-sm-6 important" dir="rtl" lang="fa"><span className="flaticon-account " lang="fa"/>احسان
-                        خامس پناه
-                    </div>
-                    <div className="col-sm-6 info" dir="rtl" lang="fa">
-                        <ul lang="fa">
-                            <li lang="fa"><i className="flaticon-phone" dir="rtl"/>
-                                ۰۹۱۲۳۴۵۶۷۸۹
-                            </li>
-                            <li lang="fa"><i className="flaticon-mail" dir="rtl"/>
-                                ekhamespanah@yahoo.com
-                            </li>
-                            <li lang="fa"><i className="flaticon-card" dir="rtl"/>
-                                ۱۰۰۰۰۰ تومان
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <UserInfo/>
                 <div className="container-fluid new-content" lang="fa">
-
                     <div className="container-fluid col-sm-10 box white-back" lang="fa">
-            <span className="container btn-group col-sm-8 choices pink-back" dir="rtl" lang="fa">
-        <button type="button" className="btn btn-primary orders black-font white-back" dir="rtl"
-                onClick={this.goToCredit} lang="fa">افزایش اعتبار</button>
-        <button type="button" className="btn btn-primary credit white-font pink-back left-border" dir="rtl"
-                onClick={this.goToOrders} lang="fa">سفارش ها</button>
-    </span>
+                        <Choices location="orders"/>
                         <br className="clear"/>
                         <form className="col-sm-12 new-inner" action="post" dir="rtl" lang="fa">
                             <div className="col-sm-9 row " lang="fa">
                                 <div className="restaurant-order black-font count-width" lang="fa">۱</div>
                                 <div className="restaurant-name-width restaurant-order black-font" lang="fa">رستوران خامس</div>
                                 <div className="situation restaurant-order black-font" lang="fa">
-                    <span className="form-btn" dir="rtl" lang="fa">
-                        <button type="button" className="btn on-way" dir="rtl" disabled lang="fa">پیک در مسیر</button>
-                    </span>
-
+                                    <span className="form-btn" dir="rtl" lang="fa">
+                                        <button type="button" className="btn on-way" dir="rtl" disabled lang="fa">پیک در مسیر</button>
+                                    </span>
                                 </div>
                             </div>
                             <div className="col-sm-9 row " lang="fa">
                                 <div className="restaurant-order black-font count-width" lang="fa">۲</div>
                                 <div className="restaurant-name-width restaurant-order black-font" lang="fa">رستوران خامس</div>
                                 <div className="situation restaurant-order black-font" lang="fa">
-                    <span className="form-btn" dir="rtl" lang="fa">
-                        <button type="button" className="btn searching dark-green" dir="rtl" disabled lang="fa">در جست و جوی پیک</button>
-                    </span>
-
+                                    <span className="form-btn" dir="rtl" lang="fa">
+                                        <button type="button" className="btn searching dark-green" dir="rtl" disabled lang="fa">در جست و جوی پیک</button>
+                                    </span>
                                 </div>
                             </div>
                             <div className="col-sm-9 row " lang="fa">
