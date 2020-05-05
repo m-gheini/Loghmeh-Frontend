3e5c023e5faea2b7bf488735bcc5341f22fe7565
leila <fakheri90@gmail.com>
2020-05-05 17:49:57 +0430

    signUp logic added

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -9,9 +9,56 @@ export class SignUp extends React.Component{
     constructor(props) {
         super(props);
         this.goToSignIn = this.goToSignIn.bind(this);
+        this.handleAddUser = this.handleAddUser.bind(this);
+        this.state = {
+            userName : null,
+            familyName : null,
+            email : null,
+            phoneNumber : null,
+            password : null,
+            status : null,
+            massage :null
+        }
 
     }
-    goToSignIn(){
+    handleAddUser(event) {
+        event.preventDefault();
+        event.persist();
+        this.setState(prevState => ({userName : event.target[0].value}));
+        this.setState(prevState => ({familyName : event.target[1].value}));
+        this.setState(prevState => ({email : event.target[2].value}));
+        this.setState(prevState => ({phoneNumber : event.target[3].value}));
+        this.setState(prevState => ({password : event.target[4].value}));
+
+        var params = {
+            "password" : event.target[4].value,
+            "name" : event.target[0].value,
+            "familyName" : event.target[1].value,
+            "email" : event.target[2].value,
+            "phoneNumber" : event.target[3].value
+        };
+        var queryString = Object.keys(params).map(function(key){
+            return key + '=' + params[key]
+        }).join('&');
+        fetch('http://localhost:8080/users?'+queryString,{
+            method: 'POST' ,
+            headers: {
+                'content-length' : queryString.length,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            body: queryString
+        })
+            .then(response => response.json())
+            .then(data => {this.setState(prevState => ({status: data.errorCode,massage: data.errorMassage}))})
+            .then(data=>{
+                if(this.state.status !== 200 && this.state.status!==201 && this.state.status)
+                    window.alert(this.state.massage);
+                else
+                    ReactDOM.render(<SignIn />,document.getElementById("root"));
+            });
+    }
+
+    goToSignIn(event){
         ReactDOM.render(<SignIn />,document.getElementById("root"));
     }
     render(){
@@ -19,21 +66,22 @@ export class SignUp extends React.Component{
             <div className='whole'>
                 <Menu location="entry"/>
                 <div className="container-fluid main-content "  lang="fa">
-                    <div className="col-sm-6 sighnup-div white-back" lang="fa" id="info-box">
-                <div className="form-div" dir="rtl" lang="fa">فرم ثبت نام</div>
-                <form className="col-sm-10 signup-form" action="" dir="rtl" lang="fa" onSubmit={this.goToSignIn}>
+                    <div className="col-sm-6 sighnup-div white-back" lang="fa" id="info-box" >
+                        <div className="form-div" dir="rtl" lang="fa">فرم ثبت نام</div>
+                <form className="col-sm-10 signup-form" action="" dir="rtl" lang="fa" onSubmit={(e) => {this.handleAddUser(e)}}>
                     <br/>
                     <br/>
-                    <EntryInput htmlFor="" label="نام و نام خانوادگی:" inputType="text" placeHolder="نام و نام خانوادگی"/>
-                        <EntryInput htmlFor="email" label="پست الکترونیک:" inputType="email" placeHolder="پست الکترونیک"/>
-                        <EntryInput htmlFor="phone" label="تلفن همراه:" inputType="tel"  placeHolder="تلفن همراه"/>
-                        <EntryInput htmlFor="pwd" label="رمز عبور:" inputType="password" placeHolder="رمز عبور"/>
+                    <EntryInput htmlFor="" label="نام:" inputType="text" placeHolder="نام"/>
+                    <EntryInput htmlFor="" label="نام خانوادگی:" inputType="text" placeHolder=" نام خانوادگی"/>
+                    <EntryInput htmlFor="email" label="پست الکترونیک:" inputType="email" placeHolder="پست الکترونیک"/>
+                    <EntryInput htmlFor="phone" label="تلفن همراه:" inputType="tel"  placeHolder="تلفن همراه"/>
+                    <EntryInput htmlFor="pwd" label="رمز عبور:" inputType="password" placeHolder="رمز عبور"/>
                     <button type="submit" className="col-sm-12 btn btn-default sub-btn dark-green" dir="rtl" lang="fa">ثبت نام</button>
                     <div className="no-account" dir="rtl" lang="fa">
                         <br/>
                         <br/>
                         ثبت نام کرده اید؟
-                        <button type="button"  className="new-user" onClick={this.goToSignIn}>وارد شوید.</button>
+                        <button type="button"  className="new-user" onClick={(e) => {this.goToSignIn(e)}}>وارد شوید.</button>
                     </div>
                 </form>
                     </div>
