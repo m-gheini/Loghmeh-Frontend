7f7de4fbf37fcf6b6da37b5fb864e00fbbc361a4
leila <fakheri90@gmail.com>
2020-05-08 15:04:02 +0430

    redirect field added to register and login

--- src/js/signIn.js
+++ src/js/signIn.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from 'react-dom';
 import logo from '../Assets/LOGO.png';
 import {Loader} from "./loading";
@@ -7,14 +7,16 @@ import {Credit} from "./credit";
 import {Home} from "./home";
 import {Footer} from "./footer";
 import {Menu} from "./header";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import GoogleLogin from "react-google-login"
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 
 export class SignIn extends React.Component{
     constructor(props) {
         super(props);
         this.goToSignUp = this.goToSignUp.bind(this);
         this.signIn = this.signIn.bind(this);
+        this.state = {
+            redirect :false
+        }
     }
     goToSignUp(){
         ReactDOM.render(<SignUp/> ,document.getElementById("root"));
@@ -23,7 +25,6 @@ export class SignIn extends React.Component{
         ReactDOM.render(<Credit /> ,document.getElementById("root"));
     }
     handleEnterUser(event) {
-        console.log("UNDERSTOOD ON SUBMIT")
         event.preventDefault();
         event.persist();
         var params = {
@@ -33,7 +34,6 @@ export class SignIn extends React.Component{
         var queryString = Object.keys(params).map(function(key){
             return key + '=' + params[key]
         }).join('&');
-        console.log(queryString);
         fetch('http://localhost:8080/login?'+queryString,{
             method: 'POST' ,
             headers: {
@@ -45,8 +45,6 @@ export class SignIn extends React.Component{
             .then(response => response.json())
             .then(data => {this.setState(prevState => ({status: data.errorCode,massage: data.errorMassage}))})
             .then(data=>{
-                console.log(queryString);
-
                 if(this.state.status !== 200 && this.state.status) {
                     window.alert(this.state.massage);
                 }
@@ -54,118 +52,62 @@ export class SignIn extends React.Component{
                     localStorage.setItem("jwt",this.state.massage);
                     console.log(localStorage);
                     //window.alert(this.state.massage);
-                    ReactDOM.render(
-                        <Router>
-                            <Switch>
-                                <Route path={"/userCredit"}>
-                                    <Credit />
-                                </Route>
-                            </Switch>
-                        </Router>,document.getElementById("root"));
+                    this.state.redirect = true;
+                    this.forceUpdate();
                 }
             });
     }
-
-
-
     render() {
-        // const [email, setEmail] = useState("");
+        console.log("from login...")
+        console.log("redirect",this.state.redirect);
+        if(this.state.redirect){
+            console.log("redirect finally ",this.state.redirect);
+            return <Redirect to={"/userCredit"}/>
+        }
+        else {
+            return (
 
-        const googleSignIn = (response) => {
-            console.log(response);
-            // setEmail(response.profileObj.email);
-                const queryString = "email="+response.profileObj.email;
-                console.log(queryString);
-                fetch('http://localhost:8080/googleLogin?'+queryString,{
-                    method: 'POST' ,
-                    headers: {
-                        'content-length' : queryString.length,
-                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-                    }
-                })
-                    .then(response => response.json())
-                    .then(data => {this.setState(prevState => ({status: data.errorCode,massage: data.errorMassage}))})
-                    .then(data=>{
-                        console.log(queryString);
-                        console.log("HEREEE")
-                        console.log(this.state.message);
-                        if(this.state.status !== 200 && this.state.status) {
-
-                            window.alert(this.state.massage);
-                            ReactDOM.render(
-                                <Router>
-                                    <Switch>
-                                        <Route path={"/register"}>
-                                            <SignUp />
-                                        </Route>
-                                    </Switch>
-                                </Router>,document.getElementById("root"));
-                        }
-                        else {
-                            localStorage.setItem("jwt",this.state.massage);
-                            console.log(localStorage);
-                            //window.alert(this.state.massage);
-                            ReactDOM.render(
-                                <Router>
-                                    <Switch>
-                                        <Route path={"/userCredit"}>
-                                            <Credit />
-                                        </Route>
-                                    </Switch>
-                                </Router>,document.getElementById("root"));
-                        }
-                    });
-        };
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/userCredit"><Credit/></Route>
-                    <Route path="/register"><SignUp/></Route>
-                    <Route exact path="/login">
-                        <div className='whole'>
-                            <Menu location="entry"/>
-                            <div className="container-fluid main-content " lang="fa">
-                                <div className="col-sm-6 sighnup-div white-back" lang="fa" id="info-box">
-                                    <div className="form-div" dir="rtl" lang="fa" id="form">فرم ورود</div>
-                                    <form className="col-sm-10 signup-form" action="" dir="rtl" lang="fa"
-                                          onSubmit={(e) => {
-                                              this.handleEnterUser(e)
-                                          }}>
-                                        <EntryInput htmlFor="email" label="پست الکترونیک:" inputType="email"
-                                                    placeHolder="پست الکترونیک"/>
-                                        <EntryInput htmlFor="pwd" label="رمز عبور:" inputType="password"
-                                                    placeHolder="رمز عبور"/>
-                                        {/*<Link to={'/userCredit'}>*/}
-                                        <button type="submit" className="col-sm-12 btn btn-default sub-btn dark-green"
-                                                dir="rtl" lang="fa">ورود
-                                        </button>
-                                        {/*</Link>*/}
-                                        {/*<div className="google-signin g-signin2"/>*/}
-                                        <GoogleLogin
-                                            className="google-signin"
-                                            clientId="1033049469249-3efdhkrfvbepo8h0ma107og9eqgkd163.apps.googleusercontent.com"
-                                            buttonText="ورود به وسیله حساب کاربری گوگل  "
-                                            onSuccess = {googleSignIn}
-                                            onFailure = {googleSignIn}
-
-                                        />
-                                        <div className="no-account" dir="rtl" lang="fa">
-                                            <br/>
-                                            <br/>
-                                            کاربر جدید هستید؟
-                                            <Link to={'/register'} className="new-user">ثبت نام کنید.</Link>
-                                        </div>
-                                    </form>
+                <Router>
+                    <Switch>
+                        <Route path="/userCredit"><Credit/></Route>
+                        <Route path="/register"><SignUp/></Route>
+                        <Route exact path="/login">
+                            <div className='whole'>
+                                <Menu location="entry"/>
+                                <div className="container-fluid main-content " lang="fa">
+                                    <div className="col-sm-6 sighnup-div white-back" lang="fa" id="info-box">
+                                        <div className="form-div" dir="rtl" lang="fa" id="form">فرم ورود</div>
+                                        <form className="col-sm-10 signup-form" action="" dir="rtl" lang="fa"
+                                              onSubmit={(e) => {
+                                                  this.handleEnterUser(e)
+                                              }}>
+                                            <EntryInput htmlFor="email" label="پست الکترونیک:" inputType="email"
+                                                        placeHolder="پست الکترونیک"/>
+                                            <EntryInput htmlFor="pwd" label="رمز عبور:" inputType="password"
+                                                        placeHolder="رمز عبور"/>
+                                            <button type="submit"
+                                                    className="col-sm-12 btn btn-default sub-btn dark-green" dir="rtl"
+                                                    lang="fa">ورود
+                                            </button>
+                                            <div className="google-signin g-signin2"/>
+                                            <div className="no-account" dir="rtl" lang="fa">
+                                                <br/>
+                                                <br/>
+                                                کاربر جدید هستید؟
+                                                <Link to={'/register'} className="new-user">ثبت نام کنید.</Link>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
+                                <Footer/>
+                                {/*<Loader/>*/}
                             </div>
-                            <Footer/>
-                            {/*<Loader/>*/}
-                        </div>
 
-                    </Route>
-                </Switch>
-            </Router>
-        );
+                        </Route>
+                    </Switch>
+                </Router>
+            );
+        }
     }
 }
 export class EntryInput extends React.Component{
--- src/js/signUp.js
+++ src/js/signUp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Link, Redirect, Route, Switch} from "react-router-dom";
 import logo from '../Assets/LOGO.png';
 import {EntryInput, SignIn} from "./signIn";
 import {Menu} from "./header";
@@ -18,7 +18,8 @@ export class SignUp extends React.Component{
             phoneNumber : null,
             password : null,
             status : null,
-            massage :null
+            massage :null,
+            redirect: false
         }
 
     }
@@ -53,7 +54,9 @@ export class SignUp extends React.Component{
                     window.alert("account created successfully");
                 else {
                     window.alert(this.state.massage);
-                    ReactDOM.render(<SignIn />,document.getElementById("root"));
+                    this.state.redirect = true;
+                    this.forceUpdate();
+                    // ReactDOM.render(<SignIn />,document.getElementById("root"));
                 }
             });
     }
@@ -64,45 +67,64 @@ export class SignUp extends React.Component{
 
     }
     render(){
-        return(
-            <Router>
-                <Switch>
-                    <Route exact path="/login"><SignIn/></Route>
-                    <Route exact path="/register"  >
-                        <div className='whole'>
-                            <Menu location="entry"/>
-                            <div className="container-fluid main-content "  lang="fa">
-                                <div className="col-sm-6 sighnup-div white-back" lang="fa" id="info-box" >
-                                    <div className="form-div" dir="rtl" lang="fa">فرم ثبت نام</div>
-                                    <form className="col-sm-10 signup-form" action="" dir="rtl" lang="fa" onSubmit={(e) => {this.handleAddUser(e)}}>
-                                        <br/>
-                                        <br/>
-                                        <EntryInput htmlFor="" label="نام:" inputType="text" placeHolder="نام"/>
-                                        <EntryInput htmlFor="" label="نام خانوادگی:" inputType="text" placeHolder=" نام خانوادگی"/>
-                                        <EntryInput htmlFor="email" label="پست الکترونیک:" inputType="email" placeHolder="پست الکترونیک"/>
-                                        <EntryInput htmlFor="phone" label="تلفن همراه:" inputType="tel"  placeHolder="تلفن همراه"/>
-                                        <EntryInput htmlFor="pwd" label="رمز عبور:" inputType="password" placeHolder="رمز عبور"/>
-                                        <button type="submit" className="col-sm-12 btn btn-default sub-btn dark-green" dir="rtl" lang="fa">ثبت نام</button>
-                                        <div className="no-account" dir="rtl" lang="fa">
+        if(this.state.redirect){
+            console.log("redirect finally ",this.state.redirect);
+            return <Redirect to={"/login"}/>
+        }
+        else {
+            return (
+                <Router>
+                    <Switch>
+                        <Route exact path="/login"><SignIn/></Route>
+                        <Route exact path="/register">
+                            <div className='whole'>
+                                <Menu location="entry"/>
+                                <div className="container-fluid main-content " lang="fa">
+                                    <div className="col-sm-6 sighnup-div white-back" lang="fa" id="info-box">
+                                        <div className="form-div" dir="rtl" lang="fa">فرم ثبت نام</div>
+                                        <form className="col-sm-10 signup-form" action="" dir="rtl" lang="fa"
+                                              onSubmit={(e) => {
+                                                  this.handleAddUser(e)
+                                              }}>
                                             <br/>
                                             <br/>
-                                            ثبت نام کرده اید؟
-                                            <Link to={'/login'}>
-                                            <button type="button"  className="new-user" onClick={(e) => {this.goToSignIn(e)}}>وارد شوید.</button>
-                                            </Link>
-                                        </div>
-                                    </form>
+                                            <EntryInput htmlFor="" label="نام:" inputType="text" placeHolder="نام"/>
+                                            <EntryInput htmlFor="" label="نام خانوادگی:" inputType="text"
+                                                        placeHolder=" نام خانوادگی"/>
+                                            <EntryInput htmlFor="email" label="پست الکترونیک:" inputType="email"
+                                                        placeHolder="پست الکترونیک"/>
+                                            <EntryInput htmlFor="phone" label="تلفن همراه:" inputType="tel"
+                                                        placeHolder="تلفن همراه"/>
+                                            <EntryInput htmlFor="pwd" label="رمز عبور:" inputType="password"
+                                                        placeHolder="رمز عبور"/>
+                                            <button type="submit"
+                                                    className="col-sm-12 btn btn-default sub-btn dark-green" dir="rtl"
+                                                    lang="fa">ثبت نام
+                                            </button>
+                                            <div className="no-account" dir="rtl" lang="fa">
+                                                <br/>
+                                                <br/>
+                                                ثبت نام کرده اید؟
+                                                <Link to={'/login'}>
+                                                    <button type="button" className="new-user" onClick={(e) => {
+                                                        this.goToSignIn(e)
+                                                    }}>وارد شوید.
+                                                    </button>
+                                                </Link>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
+                                <Footer/>
+
                             </div>
-                            <Footer/>
 
-                        </div>
+                        </Route>
+                    </Switch>
+                </Router>
 
-                    </Route>
-                </Switch>
-            </Router>
-
-    );
+            );
+        }
     }
 
 }
